f87752545f4d0bffe92a4b5dd76273f1cacc3b37
chLemon <550125499@qq.com>
2020-07-27 12:20:21 +0800

    7.27

--- src/offer/No34.java
+++ src/offer/No34.java
@@ -4,6 +4,7 @@ import offer.domain.TreeNode;
 import sun.reflect.generics.tree.Tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -13,32 +14,51 @@ public class No34 {
     打印出二叉树中节点值的和为输入整数的所有路径。
     从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
      */
-    List<List<Integer>> result = new ArrayList<>();
-    List<Integer> path = new ArrayList<>();
-    int sum;
+//    LinkedList<List<Integer>> res = new LinkedList<>();
+//    LinkedList<Integer> path = new LinkedList<>();
+//
+//    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+//        recur(root, sum);
+//        return res;
+//    }
+//
+//    void recur(TreeNode root, int tar) {
+//        if (root == null) return;
+//        path.add(root.val);
+//        tar -= root.val;
+//        if (tar == 0 && root.left == null && root.right == null)
+//            res.add(new LinkedList(path));
+//        recur(root.left, tar);
+//        recur(root.right, tar);
+//        path.removeLast();
+//    }
+    //大佬太牛逼了！
+
+    List<List<Integer>> result = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        this.sum = sum;
-        int pathSum = 0;
-        preOrder(root, pathSum);
+        preOrder(root, sum);
         return result;
     }
 
-    public void preOrder(TreeNode node, int pathSum) {
-        pathSum += node.val;
-        if (pathSum > sum) {
-
+    public void preOrder(TreeNode node, int target) {
+        if (node == null) {
+            return;
         }
-        if (pathSum == sum) {
-
+        path.add(node.val);
+        target -= node.val;
+        if (target == 0 && node.left == null && node.right == null) {
+            result.add(new LinkedList(path));
         }
         if (node.left != null) {
-            preOrder(node.left, pathSum);
+            preOrder(node.left, target);
         }
         if (node.right != null) {
-            preOrder(node.right, pathSum);
+            preOrder(node.right, target);
         }
-        return;
+        path.removeLast();
+
     }
 
 }
---
+++ src/offer/No35.java
@@ -0,0 +1,84 @@
+package offer;
+
+import offer.domain.No35.Node;
+
+import java.util.HashMap;
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+public class No35 {
+    /*
+    请实现 copyRandomList 函数，
+    复制一个复杂链表。
+    在复杂链表中，
+    每个节点除了有一个 next 指针指向下一个节点，
+    还有一个 random 指针指向链表中的任意节点或者 null。
+     */
+    public Node copyRandomList1(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+
+    }
+
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Node cur = head;
+        while (cur != null) {
+            Node curCopy = new Node(cur.val);
+            curCopy.next = cur.next;
+            cur.next = curCopy;
+            cur = curCopy.next;
+        }
+        cur = head;
+        while (cur != null) {
+            Node curCopy = cur.next;
+            if (cur.random == null) {
+                curCopy.random = null;
+            } else {
+                curCopy.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        Node copyHead = head.next;
+        Node copyNode = copyHead;
+        while (head != null) {
+            if (copyNode.next == null) {
+                head.next = null;
+            } else {
+                head.next = copyNode.next;
+                copyNode.next = copyNode.next.next;
+            }
+            head = head.next;
+            copyNode = copyNode.next;
+
+        }
+        return copyHead;
+    }
+
+}

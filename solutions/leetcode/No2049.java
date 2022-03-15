6d34fe7d679d5ad8e9c40a2452045d10f51a17c5
weijunchen <weijunchen@sohu-inc.com>
2022-03-15 16:16:01 +0800

    No599

---
+++ solutions/leetcode/No2049.java
@@ -0,0 +1,22 @@
+//package leetcode;
+//
+//public class No2049 {
+//
+//	public int countHighestScoreNodes(int[] parents) {
+//	}
+//	
+//	long calCost(int[] parents) {
+//		// 压缩
+//		for (int i = 0; i < parents.length; i++) {
+//			// 找到我们的老大
+//			while (parents[i] != -1) {
+//				parents[i] = 
+//			}
+//			parents[i]
+//			while ()
+//			if (parents[i]){
+//				
+//			}
+//		}
+//	}
+//}

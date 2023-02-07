f385d4bf3457c8efd7c9c2ca3373ba76ec11ddc6
weijunchen <weijunchen@sohu-inc.com>
2023-02-07 09:14:55 +0800

    0207

--- test/test.java
+++ test/test.java
@@ -11,11 +11,7 @@ public class test {
 
     @Test
     public void test() {
-        UserOnlyBrokerIdDTO d1 = new UserOnlyBrokerIdDTO(1L, 1L);
-        UserOnlyBrokerIdDTO d2 = new UserOnlyBrokerIdDTO(2L, 1L);
-        List<UserOnlyBrokerIdDTO> list = Arrays.asList(d1, d2);
-        Map<Long, Set<Long>> map = list.stream().collect(Collectors.groupingBy(UserOnlyBrokerIdDTO::getBrokerId, Collectors.mapping(UserOnlyBrokerIdDTO::getUserId, Collectors.toSet())));
-        System.out.println(map);
+        System.out.println((-3)/2);
 
     }
 }

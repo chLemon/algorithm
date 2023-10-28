fc9685694887a1ea8784222a5692b21ee9bfa451
chLemon <550125499@qq.com>
2023-10-28 17:54:22 +0800

    新增了一个类

--- src/test/java/leetcode/No1.java
+++ src/test/java/leetcode/No1.java
@@ -5,19 +5,21 @@ import java.util.Map;
 
 public class No1 {
 
-	class Solution {
-		public int[] twoSum(int[] nums, int target) {
-			Map<Integer, Integer> map = new HashMap<>();
-			for (int i = 0; i < nums.length; i++) {
-				int tar = target - nums[i];
-				if (map.containsKey(tar)) {
-					return new int[]{map.get(tar), i};
-				}else {
-					map.put(nums[i], i);
-				}
-			}
-			return null;	
-		}
-	}
-	
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] res = new int[2];
+            Map<Integer, Integer> number2Index = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                if (number2Index.containsKey(target - num)) {
+                    res[0] = number2Index.get(target - num);
+                    res[1] = i;
+                    return res;
+                }
+                number2Index.put(num, i);
+            }
+            return res;
+        }
+    }
+
 }

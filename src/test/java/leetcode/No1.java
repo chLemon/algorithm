113cfea3285b7375d451a36c00eed7539aad9a2e
chLemon <550125499@qq.com>
2023-10-29 17:52:20 +0800

    二分查找

--- src/test/java/leetcode/No1.java
+++ src/test/java/leetcode/No1.java
@@ -5,21 +5,19 @@ import java.util.Map;
 
 public class No1 {
 
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            int[] res = new int[2];
-            Map<Integer, Integer> number2Index = new HashMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                int num = nums[i];
-                if (number2Index.containsKey(target - num)) {
-                    res[0] = number2Index.get(target - num);
-                    res[1] = i;
-                    return res;
-                }
-                number2Index.put(num, i);
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> number2Index = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (number2Index.containsKey(target - num)) {
+                res[0] = number2Index.get(target - num);
+                res[1] = i;
+                return res;
             }
-            return res;
+            number2Index.put(num, i);
         }
+        return res;
     }
 
 }

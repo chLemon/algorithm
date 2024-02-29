bd45e59ce28c7c5baa9a60f8af58530e64b9f7cd
weijunchen <weijunchen@sohu-inc.com>
2024-02-29 19:48:55 +0800

    No459

--- src/main/java/leetcode/No459.java
+++ src/main/java/leetcode/No459.java
@@ -4,27 +4,27 @@ public class No459 {
 
     public static void main(String[] args) {
         No459 no = new No459();
-        System.out.println(no.repeatedSubstringPattern("abacababacab"));
-        // abacab
-        // abacab
+//        System.out.println(no.repeatedSubstringPattern("abab"));    // t
+//        System.out.println(no.repeatedSubstringPattern("aba"));     // f
+//        System.out.println(no.repeatedSubstringPattern("abacababacab"));    // t
+        System.out.println(no.repeatedSubstringPattern("abac"));    // f
     }
 
     public boolean repeatedSubstringPattern(String s) {
-        for (int i = 1; i < s.length(); i++) {
-            while (i < s.length() && s.charAt(i) != s.charAt(0)) i++;
-            // i和第一个字符一样
-            int repeatStartIndex = i;
-            int j = 0;
-            while (i < s.length() && s.charAt(i) == s.charAt(j)) {
-                i++;
-                j++;
-            }
-            if (repeatStartIndex + j == s.length() && j >= repeatStartIndex && s.length() % repeatStartIndex == 0) {
-                return true;
+        // 尝试构建next表
+        int[] next = new int[s.length() + 1];
+        int i = 0;
+        int t = next[0] = -1;
+        while (i < s.length()) {
+            if (t < 0 || s.charAt(i) == s.charAt(t)) {
+                next[++i] = ++t;
+            } else {
+                t = next[t];
             }
         }
-        return false;
+        // next表构建完成
+        int subMax = next[s.length()];
+        return subMax != 0 && subMax % (s.length() - subMax) == 0;
     }
-    
 
 }

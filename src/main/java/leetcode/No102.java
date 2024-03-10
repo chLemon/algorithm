15d673f95fe3a3bf1c7dc083b96f133b13f96851
weijunchen <weijunchen@sohu-inc.com>
2024-03-10 18:37:14 +0800

    No102

---
+++ src/main/java/leetcode/No102.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No102 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int size = queue.size();
+            // 这里还有一种写法：    for(int i = queue.size(); i > 0; i--)  这样i一开始被赋值了，queue.size()改变也无所谓
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                tmp.add(poll.val);
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+
+            res.add(tmp);
+        }
+
+        return res;
+
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

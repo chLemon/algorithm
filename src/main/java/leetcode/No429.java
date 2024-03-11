35c43e895420816a5957b140046c80a5722e3a66
weijunchen <weijunchen@sohu-inc.com>
2024-03-11 11:40:56 +0800

    No429

---
+++ src/main/java/leetcode/No429.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No429 {
+
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                Node poll = queue.poll();
+                tmp.add(poll.val);
+                if (poll.children != null) {
+                    poll.children.forEach(queue::offer);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}

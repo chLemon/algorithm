8d91f5449876ad72ec2d18591c471023be7fd3be
weijunchen <weijunchen@sohu-inc.com>
2024-03-11 19:21:38 +0800

    No101

---
+++ src/main/java/leetcode/No101.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+public class No101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 左右两个子树完全对称相等，可以BFS也可以DFS
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return dfs(left.right, right.left) && dfs(left.left, right.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

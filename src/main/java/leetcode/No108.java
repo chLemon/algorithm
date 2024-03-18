6d53c840b8f781f5829b554405d184246c8a96c3
chLemon <550125499@qq.com>
2024-03-18 22:07:31 +0800

    No108

---
+++ src/main/java/leetcode/No108.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+public class No108 {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        No108 no = new No108();
+        TreeNode node = no.sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (end <= start) {
+            return null;
+        }
+        if (end - start == 1) {
+            return new TreeNode(nums[start]);
+        }
+        int mid = start + (end - start) / 2;
+        return new TreeNode(nums[mid],
+                sortedArrayToBST(nums, start, mid),
+                sortedArrayToBST(nums, mid + 1, end));
+    }
+}

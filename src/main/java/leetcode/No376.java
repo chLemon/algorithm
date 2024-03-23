32e13ae6133314ce87c26298bd854ad8c1693ef0
weijunchen <weijunchen@sohu-inc.com>
2024-03-23 14:42:56 +0800

    No376

--- src/main/java/leetcode/No376.java
+++ src/main/java/leetcode/No376.java
@@ -6,10 +6,8 @@ public class No376 {
         int i = no.wiggleMaxLength2(new int[]{3, 3, 3, 2, 5});
     }
 
+    // todo cwj 这个题最后用dp再做一次
     public int wiggleMaxLength2(int[] nums) {
-        if (nums.length == 1) {
-            return 1;
-        }
         int pre = 0;
         int count = 1;
         for (int i = 1; i < nums.length; i++) {

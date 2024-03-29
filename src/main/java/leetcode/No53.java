2569a31004ec6b417d98d280cb2856819756a586
weijunchen <weijunchen@sohu-inc.com>
2024-03-29 11:29:22 +0800

    No53

--- src/main/java/leetcode/No53.java
+++ src/main/java/leetcode/No53.java
@@ -1,14 +1,36 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class No53 {
-    public int maxSubArray(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1] > 0 ? nums[i - 1] : 0;
-        }
-        int result = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            result = nums[i] > result ? nums[i] : result;
-        }
-        return result;
+
+    public static void main(String[] args) {
+        No53 no = new No53();
+        no.maxSubArray2(new int[]{5, 4, -1, 7, 8});
     }
+
+    public int maxSubArray(int[] nums) {
+        // 以i结尾的最大子数组和
+        int[] f = new int[nums.length];
+        f[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            f[i] = f[i - 1] <= 0 ? nums[i] : f[i - 1] + nums[i];
+        }
+        return Arrays.stream(f).max().orElse(0);
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int[] f = new int[nums.length + 1];
+        int min = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= nums.length; i++) {
+            f[i] = f[i - 1] + nums[i - 1];
+            max = Math.max(max, f[i] - min);
+            min = Math.min(min, f[i]);
+        }
+        return max;
+    }
+
 }

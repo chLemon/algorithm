56f2f650fa205513b65d297e3d0d6f357b65017a
weijunchen <weijunchen@sohu-inc.com>
2024-03-29 14:07:15 +0800

    No115

--- src/main/java/leetcode/No392.java
+++ src/main/java/leetcode/No392.java
@@ -15,4 +15,26 @@ public class No392 {
         }
         return si == s.length();
     }
+
+
+    public boolean isSubsequence2(String s, String t) {
+        int length1 = s.length();
+        int length2 = t.length();
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        if (dp[length1][length2] == length1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }

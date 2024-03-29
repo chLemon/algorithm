1a3ce6764ee55dae892ff5f375b36e4e12b7b75b
weijunchen <weijunchen@sohu-inc.com>
2024-03-29 12:02:20 +0800

    No392

---
+++ src/main/java/leetcode/No392.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class No392 {
+    
+    public boolean isSubsequence(String s, String t) {
+        int si = 0;
+        int ti = 0;
+        while (si < s.length() && ti < t.length()) {
+            if (s.charAt(si) == t.charAt(ti)) {
+                si++;
+                ti++;
+            } else {
+                ti++;
+            }
+        }
+        return si == s.length();
+    }
+}

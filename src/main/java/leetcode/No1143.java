371ef186dc157110eefe2cd4c8c0beaa6019bf11
weijunchen <weijunchen@sohu-inc.com>
2024-03-29 11:02:27 +0800

    No1035

--- src/main/java/leetcode/No1143.java
+++ src/main/java/leetcode/No1143.java
@@ -10,7 +10,6 @@ public class No1143 {
 
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] f = new int[text1.length() + 1][text2.length() + 1];
-        int max = -0x3f3f3f;
 
         for (int i = 1; i <= text1.length(); i++) {
             char c1 = text1.charAt(i - 1);
@@ -21,10 +20,9 @@ public class No1143 {
                 } else {
                     f[i][j] = f[i - 1][j - 1] + 1;
                 }
-                max = Math.max(max, f[i][j]);
             }
         }
-        return max;
+        return f[text1.length()][text2.length()];
     }
 
 }

35c5c830cc88e6b078d7e034ad26653d5853f5a7
weijunchen <weijunchen@sohu-inc.com>
2024-04-03 20:33:23 +0800

    No189 + No2810

---
+++ src/main/java/leetcode/No2810.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class No2810 {
+
+    public static void main(String[] args) {
+        No2810 no = new No2810();
+        String s = no.finalString("string");
+        System.out.println(s);
+    }
+
+    public String finalString(String s) {
+        int index = 0;
+        char[] chars = new char[s.length()];
+        for (int i = 0; i < chars.length; i++) {
+            if (s.charAt(i) != 'i') {
+                chars[index++] = s.charAt(i);
+            } else {
+                reverse(chars, 0, index - 1);
+            }
+        }
+        return new String(chars, 0, index);
+    }
+
+
+    private void reverse(char[] chars, int start, int end) {
+        while (start <= end) {
+            char tmp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+}

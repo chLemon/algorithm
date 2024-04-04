202a07c9baa5c57ccc13b2a3659b68a66135e46c
weijunchen <weijunchen@sohu-inc.com>
2024-04-04 18:49:55 +0800

    No922

---
+++ src/main/java/leetcode/No922.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class No922 {
+
+    public int[] sortArrayByParityII(int[] nums) {
+        int findOddInEven = 0;
+        int findEvenInOdd = 1;
+        while (findOddInEven < nums.length && findEvenInOdd < nums.length) {
+            while (findOddInEven < nums.length && (nums[findOddInEven] & 1) == 0) {
+                findOddInEven += 2;
+            }
+            while (findEvenInOdd < nums.length && (nums[findEvenInOdd] & 1) == 1) {
+                findEvenInOdd += 2;
+            }
+            if (findOddInEven >= nums.length || findEvenInOdd >= nums.length) {
+                break;
+            }
+            // 交换
+            int tmp = nums[findOddInEven];
+            nums[findOddInEven] = nums[findEvenInOdd];
+            nums[findEvenInOdd] = tmp;
+            findOddInEven += 2;
+            findEvenInOdd += 2;
+        }
+        return nums;
+    }
+
+}

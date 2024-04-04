f7fbee7540a27e842ed8ed285a491bee7108ba34
weijunchen <weijunchen@sohu-inc.com>
2024-04-04 18:54:07 +0800

    No35

--- src/main/java/leetcode/No35.java
+++ src/main/java/leetcode/No35.java
@@ -2,9 +2,18 @@ package leetcode;
 
 public class No35 {
 
-//    public int searchInsert(int[] nums, int target) {
-//
-//
-//    }
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
 
 }

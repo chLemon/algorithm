38bcb79d000d8e9757914e80651e3e053148dcca
weijunchen <weijunchen@sohu-inc.com>
2024-04-10 14:02:39 +0800

    代码随心录二刷-二叉树的层次遍历

---
+++ src/main/java/programmercarl/No102.java
@@ -0,0 +1,31 @@
+package programmercarl;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No102 {
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> layer = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode cur = queue.poll();
+                layer.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            res.add(layer);
+        }
+        return res;
+    }
+
+}
---
+++ src/main/java/programmercarl/No107.java
@@ -0,0 +1,29 @@
+package programmercarl;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class No107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> layer = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode cur = queue.poll();
+                layer.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            res.addFirst(layer);
+        }
+        return res;
+    }
+
+}

ad02765f1c988cbc7068239731c138be8be0204a
chLemon <550125499@qq.com>
2024-04-20 00:35:20 +0800

    0x3f-链表-反转系列

---
+++ src/main/java/inf/No24.java
@@ -0,0 +1,25 @@
+package inf;
+
+import java.util.List;
+
+public class No24 {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        ListNode one = pre.next;
+        ListNode two = one == null ? null : one.next;
+        while (one != null && two != null) {
+            ListNode nxt = two.next;
+            pre.next = two;
+            two.next = one;
+            one.next = nxt;
+
+            pre = one;
+            one = pre.next;
+            two = one == null ? null : one.next;
+        }
+        return dummy.next;
+    }
+
+}
---
+++ src/main/java/inf/No25.java
@@ -0,0 +1,39 @@
+package inf;
+
+public class No25 {
+
+    public static void main(String[] args) {
+        new No25().reverseKGroup(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null) {
+            cur = cur.next;
+            count++;
+        }
+        int valid = count / k * k;
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode p0 = dummy;
+        while (valid > 0) {
+            ListNode pre = null;
+            cur = p0.next;
+            for (int i = 0; i < k; i++) {
+                ListNode next = cur.next;
+                cur.next = pre;
+
+                pre = cur;
+                cur = next;
+            }
+            ListNode p1 = p0.next;
+            p1.next = cur;
+            p0.next = pre;
+            valid -= k;
+            p0 = p1;
+        }
+        return dummy.next;
+    }
+
+}

3026c23ebe729cec7ebd4e1dd55ac73564be27e2
weijunchen <weijunchen@sohu-inc.com>
2024-04-20 13:32:15 +0800

    0x3f-链表-删除系列

---
+++ src/main/java/inf/No19.java
@@ -0,0 +1,22 @@
+package inf;
+
+public class No19 {
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        for (int i = 0; i < n + 1; i++) {
+            fast = fast.next;
+        }
+        ListNode slow = dummy;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+}
---
+++ src/main/java/inf/No237.java
@@ -0,0 +1,10 @@
+package inf;
+
+public class No237 {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}

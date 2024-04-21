9fa96687ef7a5144d736bdb4c367d4a467260383
weijunchen <weijunchen@sohu-inc.com>
2024-04-21 12:14:42 +0800

    394周赛

---
+++ src/main/java/contest/weekly/w394/No2.java
@@ -0,0 +1,42 @@
+package contest.weekly.w394;
+
+public class No2 {
+
+    public static void main(String[] args) {
+        new No2().numberOfSpecialChars("aaAbcBC");
+    }
+
+    public int numberOfSpecialChars(String word) {
+        // 初始为0，出现了小写字母，记为1，如果出现了大写字母1可以转为2，如果又出现了小写，则为-1
+        int[] flag = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                int j = c - 'a';
+
+                if (flag[j] == 0) {
+                    flag[j] = 1;
+                } else if (flag[j] == 2) {
+                    flag[j] = -1;
+                }
+            } else {
+                int j = c - 'A';
+
+                if (flag[j] == 0) {
+                    flag[j] = -1;
+                } else if (flag[j] == 1) {
+                    flag[j] = 2;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (flag[i] == 2) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+}
---
+++ src/main/java/contest/weekly/w394/No4.java
@@ -0,0 +1,93 @@
+package contest.weekly.w394;
+
+import java.util.*;
+
+public class No4 {
+
+    public static void main(String[] args) {
+        boolean[] res = new No4().findAnswer(4, new int[][]
+                {{2, 0, 1}, {0, 1, 1}, {0, 3, 4}, {3, 2, 2}}
+        );
+        System.out.println(Arrays.toString(res));
+    }
+
+    public boolean[] findAnswer(int n, int[][] edges) {
+        int[] nodeInfo = new int[n];
+        Arrays.fill(nodeInfo, -1);
+        Map<Integer, List<int[]>> node2nodeAndCost = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int[] edge = edges[i];
+            node2nodeAndCost.computeIfAbsent(edge[0], k -> new ArrayList<>())
+                    .add(new int[]{edge[1], edge[2], i});
+            node2nodeAndCost.computeIfAbsent(edge[1], k -> new ArrayList<>())
+                    .add(new int[]{edge[0], edge[2], i});
+        }
+
+        Set<Integer> pPoints = new HashSet<>();
+        PriorityQueue<int[]> tPoints = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+
+        // 访问节点0
+        pPoints.add(0);
+        nodeInfo[0] = 0;
+        List<int[]> nodeAndCost = node2nodeAndCost.getOrDefault(0, new ArrayList<>());
+        for (int[] pair : nodeAndCost) {
+            tPoints.add(new int[]{pair[0], pair[1]});
+        }
+        // 开始不断访问T节点
+        while (!tPoints.isEmpty()) {
+            // 取出最小的节点
+            int[] minNode = tPoints.poll();
+
+            if (minNode[0] == n - 1) {
+                // 结束
+                pPoints.add(n - 1);
+                nodeInfo[n - 1] = minNode[1];
+                break;
+            }
+
+
+            // 由于优先级队列会保存一些冗余数据，跳过
+            int minNodeValue = minNode[0];
+            if (pPoints.contains(minNodeValue)) {
+                continue;
+            }
+            // 变为p
+            pPoints.add(minNodeValue);
+            nodeInfo[minNodeValue] = minNode[1];
+            // 修改p周围的t
+            List<int[]> neighbor = node2nodeAndCost.getOrDefault(minNodeValue, new ArrayList<>());
+            for (int[] nei : neighbor) {
+                if (pPoints.contains(nei[0])) {
+                    continue;
+                }
+                // 修改值，加入t
+                tPoints.offer(new int[]{nei[0], nei[1] + minNode[1]});
+            }
+        }
+
+        // 从n-1 开始
+        boolean[] res = new boolean[edges.length];
+        if (nodeInfo[n - 1] < 0) return res;
+        Queue<Integer> neigh = new LinkedList<>();
+        neigh.offer(n - 1);
+        while (!neigh.isEmpty()) {
+            Integer node = neigh.poll();
+            // 是从哪里走到这个节点的
+            List<int[]> froms = node2nodeAndCost.getOrDefault(node, new ArrayList<>());
+            for (int[] from : froms) {
+                if (pPoints.contains(from[0])) {
+                    int fromValue = nodeInfo[from[0]];
+                    int cost = from[1];
+                    if (fromValue + cost == nodeInfo[node]) {
+                        // 可以从这里走来
+                        res[from[2]] = true;
+                        // 接下来访问这里
+                        neigh.offer(from[0]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}

a3cc7a25f03625df5b69e320a3a9d6817f2e2ded
weijunchen <weijunchen@sohu-inc.com>
2024-04-22 11:34:11 +0800

    0x3f-二叉树与递归-灵活运用

---
+++ src/main/java/inf/No101.java
@@ -0,0 +1,17 @@
+package inf;
+
+public class No101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) return false;
+        return isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+    }
+
+}
---
+++ src/main/java/inf/No1026.java
@@ -0,0 +1,27 @@
+package inf;
+
+public class No1026 {
+
+    int V = 0;
+
+    public int maxAncestorDiff(TreeNode root) {
+        int value = root.val;
+        maxAncestorDiff(root.left, value, value);
+        maxAncestorDiff(root.right, value, value);
+        return V;
+    }
+
+    private void maxAncestorDiff(TreeNode root, int pathMin, int pathMax) {
+        if (root == null) return;
+        int value = root.val;
+        int v1 = Math.abs(value - pathMin);
+        int v2 = Math.abs(value - pathMax);
+        V = Math.max(V, v1);
+        V = Math.max(V, v2);
+        pathMin = Math.min(pathMin, value);
+        pathMax = Math.max(pathMax, value);
+        maxAncestorDiff(root.left, pathMin, pathMax);
+        maxAncestorDiff(root.right, pathMin, pathMax);
+    }
+
+}

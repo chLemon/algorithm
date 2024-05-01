abce16d634779ee81986aa499fe66f018dd40057
weijunchen <weijunchen@sohu-inc.com>
2024-05-01 14:37:33 +0800

    0x3f-状态机DP-买卖股票系列

---
+++ src/main/java/inf/No188.java
@@ -0,0 +1,35 @@
+package inf;
+
+public class No188 {
+
+    public static void main(String[] args) {
+        new No188().maxProfit(2, new int[]{3, 2, 6, 5, 0, 3});
+    }
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int INF = 0x3f3f3f;
+
+        // f[i][k][0/1] 表示，在第i天结束，操作了k笔，持有1 / 未持有0 时的利润
+        int[][][] f = new int[n][k + 1][2];
+
+        // init，买的时候扣减次数
+        // 第0天，任意次操作下，未持有的利润都是0；持有在1-k次的时候为-prices，0次为-INF
+        // 在任意天，0次操作情况下，未持有利润为0；持有都是-INF
+        for (int i = 1; i < k + 1; i++) {
+            f[0][i][1] = -prices[0];
+        }
+        for (int i = 0; i < n; i++) {
+            f[i][0][1] = -INF;
+        }
+        for (int i = 1; i < n; i++) {
+            int x = prices[i];
+            for (int j = 1; j <= k; j++) {
+                f[i][j][0] = Math.max(f[i - 1][j][0], f[i - 1][j][1] + x);
+                f[i][j][1] = Math.max(f[i - 1][j][1], f[i - 1][j - 1][0] - x);
+            }
+        }
+        return f[n - 1][k][0];
+    }
+
+}
---
+++ src/main/java/inf/No309.java
@@ -0,0 +1,29 @@
+package inf;
+
+public class No309 {
+
+    public static void main(String[] args) {
+        new No309().maxProfit(new int[]{1, 2, 4});
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        // 定义 f[i][0/1] 表示 第i天结束时，持有1 / 未持有0 的情况下，利润的最大值
+        int[][] f = new int[n][2];
+        f[0][0] = 0;
+        f[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            int x = prices[i];
+            f[i][0] = Math.max(f[i - 1][0], f[i - 1][1] + x);
+            // 由于卖出股票的第二天不能买入股票。所以今天如果持有股票，只可能是从前天未持有的状态转移而来：
+            // 前天卖出昨天冷冻 / 前天就未持有，昨天依旧未持有
+            // i - 2 < 0 时，昨天是第一天，今天持有，昨天未持有今天买入 / 昨天买入今天不变
+            f[i][1] = Math.max(f[i - 1][1],
+                    (i - 2 < 0 ? f[i - 1][0] : f[i - 2][0]) - x);
+        }
+
+        return f[n - 1][0];
+    }
+
+}

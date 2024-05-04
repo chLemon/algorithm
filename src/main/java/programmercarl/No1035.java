cbe2f0162ce61ace42bd11642d51bb478a087522
chLemon <550125499@qq.com>
2024-05-04 12:54:26 +0800

    代码随想录二刷-动态规划

---
+++ src/main/java/programmercarl/No1035.java
@@ -0,0 +1,24 @@
+package programmercarl;
+
+public class No1035 {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[][] f = new int[m + 1][n + 1];
+        int max = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    f[i][j] = 1 + f[i - 1][j - 1];
+                } else {
+                    f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                }
+                max = Math.max(max, f[i][j]);
+            }
+        }
+        return max;
+    }
+
+}
---
+++ src/main/java/programmercarl/No674.java
@@ -0,0 +1,22 @@
+package programmercarl;
+
+import java.util.Arrays;
+
+public class No674 {
+
+    public int findLengthOfLCIS(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n];
+
+        f[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                f[i] = f[i - 1] + 1;
+            } else {
+                f[i] = 1;
+            }
+        }
+        return Arrays.stream(f).max().orElse(1);
+    }
+
+}

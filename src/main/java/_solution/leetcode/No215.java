af3b0c65d98306f84eb346492de5c4689ce2995c
chLemon <550125499@qq.com>
2024-05-09 23:16:29 +0800

    No215

--- src/main/java/_solution/leetcode/No215.java
+++ src/main/java/_solution/leetcode/No215.java
@@ -3,15 +3,15 @@ package _solution.leetcode;
 import java.util.Random;
 
 public class No215 {
+    public static void main(String[] args) {
+        System.out.println(new No215().findKthLargest(new int[]{6, 2, 1, 5, 3, 4}, 2));
+    }
+
     int[] nums;
     int n;
-    Random random = new Random();
+    Random random = new Random(System.currentTimeMillis());
 
     // 直接建堆，O(n) 但是选k个，k * logn
-
-    public static void main(String[] args) {
-        new No215().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4);
-    }
 
     // 快排变种 k-select 期望O(n)
     public int findKthLargest(int[] nums, int k) {
@@ -24,22 +24,22 @@ public class No215 {
     private int partition(int l, int r, int k) {
         if (l == r) return nums[l];
         // 从 l - r 里随机选一个，作为轴点
-        int candidateIndex = l + random.nextInt(r - l + 1);
-        swap(l, candidateIndex);
+//        int candidateIndex = l + random.nextInt(r - l + 1);
+//        swap(l, candidateIndex);
 
         // LGU
         int x = nums[l];
         int m = l;
         for (int i = l + 1; i <= r; i++) {
             if (nums[i] < x) {
-                swap(nums[++m], nums[i]);
+                swap(++m, i);
             }
         }
-        swap(nums[l], nums[m]);
+        swap(l, m);
         // low 就是 轴点
         if (m == k) return nums[m];
         if (m > k) return partition(l, m - 1, k);
-        return partition(m + 1, r, k - m - 1);
+        return partition(m + 1, r, k);
     }
 
     private void swap(int i, int j) {
@@ -48,5 +48,17 @@ public class No215 {
         nums[j] = t;
     }
 
+    // 霍尔分区方案
+    int qselect(int l, int r, int k) {
+        if (l == r) return nums[k];
+        int x = nums[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (nums[i] < x);
+            do j--; while (nums[j] > x);
+            if (i < j) swap(i, j);
+        }
+        if (k <= j) return qselect(l, j, k);
+        else return qselect(j + 1, r, k);
+    }
 
 }

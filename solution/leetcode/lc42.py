15c84cc3365972e922b31de2ad867ad755e2b784
chLemon <550125499@qq.com>
2025-10-12 22:19:34 +0800

    update

--- solution/leetcode/lc42.py
+++ solution/leetcode/lc42.py
@@ -2,6 +2,7 @@
 # O(n) O(1)
 # 相向双指针
 
+
 class Solution:
     def trap1(self, height: List[int]) -> int:
         # 每一块位置能接到的雨水，可以看作是两块木板挡下的，左侧木板是左侧最高的值，这个值可以算上当前值，右侧同理
@@ -26,18 +27,18 @@ class Solution:
     def trap2(self, height: List[int]) -> int:
         # 双指针，在某个时刻，可以确定的是，双指针之间可能有更高的墙，但是两边最终的墙的高度却不会再变矮了。所以短的那个边的值其实就已经确定了。
         n = len(height)
-        count = left = l_height = r_height = 0
+        ans = left = l_max = r_max = 0
         right = n - 1
         while left <= right:
-            if l_height < r_height:
-                count += max(0, l_height - height[left])
-                l_height = max(l_height, height[left])
+            l_max = max(l_max, height[left])
+            r_max = max(r_max, height[right])
+            if l_max < r_max:
+                ans += l_max - height[left]
                 left += 1
             else:
-                count += max(0, r_height - height[right])
-                r_height = max(r_height, height[right])
+                ans += r_max - height[right]
                 right -= 1
-        return count
+        return ans
 
     def trap(self, height: List[int]) -> int:
         return self.trap2(height)

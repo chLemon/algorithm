2ccf2b21df322eaf773b2eea1493f74cd617c02e
chLemon <550125499@qq.com>
2026-01-18 15:43:49 +0800

    update

--- solution/leetcode/lc131.py
+++ solution/leetcode/lc131.py
@@ -3,11 +3,33 @@
 # Tags:
 
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:
+
+    # 选和不选
+    def method1(self, s: str) -> List[List[str]]:
         ans = []
+        n = len(s)
+        path = []
 
-        def isPalindrome():
+        def dfs(i, start):
+            if i == n:
+                ans.append(path.copy())
+                return
+
+            # 不选
+            dfs(i + 1, start)
+
+            # 选
+            # 判断是否是回文串
+            t = s[start: i]
+            if t == t[::-1]:
+                path.append(s[start: i])
+                dfs(i + 1, i)
+                path.pop()
+
+        dfs(0, 0)
+        return ans
 
 
-        def dfs(i):
-            if isPalindrome():
+
+    def partition(self, s: str) -> List[List[str]]:
+        return self.method1(s)
